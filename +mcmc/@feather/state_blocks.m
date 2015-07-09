8179ed6578964068ee484def735f159db90af4f2
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-07-09 11:53:58 +1000

    Added block numbers to output

--- +mcmc/@feather/state_blocks.m
+++ +mcmc/@feather/state_blocks.m
@@ -1,4 +1,4 @@
-function [blocks,block_color] = state_blocks(self)
+function [blocks,block_color,state_numbers] = state_blocks(self)
 	% For a given state_str
 	% return a matrix of the states and their corresponding colours
 	% which can be used as indexes for plotting
@@ -10,5 +10,5 @@ function [blocks,block_color] = state_blocks(self)
 	blocks = [trans(1:end-1),trans(2:end)];
     cdata = braintrack_utils.state_cdata();
 	block_color = cdata(state_number(blocks(:,1)),:);
-
+	state_numbers = state_number(blocks(:,1));
 	
