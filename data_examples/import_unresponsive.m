3611fe0aaa153698358f5e9c237ee6a3570bf129
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-11-26 18:06:49 +1100

    Syncing changes

--- data_examples/import_unresponsive.m
+++ data_examples/import_unresponsive.m
@@ -1,16 +1,17 @@
-function import_unresponsive
+function import_coma
 
 	os_prefix = './psg_data/';
 
-	data_set = 'unresponsive';
-	idx = 1:2;
+	for j = 1:1
+		import(os_prefix,'coma',j);
+	end
 
-	parfor j = idx
-		import(os_prefix,data_set,j);
+	for j = 1:1
+		import(os_prefix,'control_coma',j);
 	end
 
 function import(os_prefix,data_set,k)
-	infile = fullfile(os_prefix,'unresponsive',sprintf('%s_%d.mat',data_set,k));
+	infile = fullfile(os_prefix,data_set,sprintf('%s_%d.mat',data_set,k));
 	fprintf('Loading: %s\n',infile);
 	fhandle = load(infile);
 	colheaders = fhandle.colheaders;
@@ -26,10 +27,10 @@ function import(os_prefix,data_set,k)
 	nspec = nspec(1:t_max_retain,:);
 
 	state_str = cell(length(t),1);
-	[state_str{:}] = deal('Unresponsive');
+	[state_str{:}] = deal(data_set);
 	state_score = fhandle.epoch_score;
 
-	outfile = fullfile(os_prefix,'unresponsive',sprintf('%s_%d_tfs.mat',data_set,k));
+	outfile = fullfile(os_prefix,data_set,sprintf('%s_%d_tfs.mat',data_set,k));
 	fprintf('Saving: %s\n',outfile);
 	save(outfile,'t','f','s','state_score','state_str','nspec','n_reject','colheaders')
 	fprintf('Done\n\n')
